8968ce77712b595bba0c4dc292c7a4a1f59c597e
talisk21 <Talisk@i.ua>
2023-06-05 20:39:27 +0300

    task-03

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://3v70jdgt00.execute-api.us-east-1.amazonaws.com/products/",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://3v70jdgt00.execute-api.us-east-1.amazonaws.com/products",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
